a3aa845b88d63c9494ee2aae14278cb10c5440f7
mohhamad-esmaili <esmaili.mohhamad@gmail.com>
2022-08-18 13:02:42 +0430

    notification implemented

---
+++ lib/service/notification_service.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+
+class NotificationService {
+  static final NotificationService _notificationService =
+      NotificationService._internal();
+
+  factory NotificationService() {
+    return _notificationService;
+  }
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  NotificationService._internal();
+
+  Future<void> initNotification() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('todo_logo');
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+    );
+  }
+
+  // this is local push notification
+  // pass arguments and get notification
+  Future<void> showNotification(int id, String title, String description,
+      DateTime dateTime, Color priority, int remindIn) async {
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id,
+      title,
+      description,
+      tz.TZDateTime.from(dateTime, tz.local).add(
+        Duration(minutes: remindIn),
+      ),
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          'todo channel',
+          'todo channel',
+          channelDescription: "todo reminder",
+          playSound: true,
+          color: priority,
+          icon: 'todo_logo',
+          importance: Importance.max,
+          priority: Priority.max,
+        ),
+      ),
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+    );
+  }
+
+  Future<void> cancelNotitication(int id) async {
+    await flutterLocalNotificationsPlugin.cancel(id);
+  }
+}
